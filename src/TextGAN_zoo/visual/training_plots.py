aca58f0fe3e5333f9b7300fec8d1d86d38022260
marluxiaboss <henrique.dasilvagameiro@epfl.ch>
2022-05-06 16:59:46 +0200

    improved plot

--- src/TextGAN_zoo/visual/training_plots.py
+++ src/TextGAN_zoo/visual/training_plots.py
@@ -1,13 +1,35 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_ratings(rating_bins, iteration):
-    plt.bar(range(1, 6), rating_bins)
-    plt.xlabel("rating")
-    plt.ylabel("number of samples")
+def plot_ratings(rating_bins):
+    """
+    Show 20 iterations of training
+    """
 
-    filename = 'ratings_bar' + str(iteration) + '.png'
-    plt.savefig(filename)
+    X = ['1 star', '2 stars', '3 stars', '4 stars', '5 stars']
+    rating_bins0 = rating_bins[0]
+    rating_bins1 = rating_bins[1]
+    rating_bins2 = rating_bins[2]
+    rating_bins3 = rating_bins[3]
+    rating_bins4 = rating_bins[4]
+
+    X_axis = np.arange(len(X))
+
+    plt.bar(X_axis - 0.2, rating_bins0 , 0.1, label='epoch 0')
+    plt.bar(X_axis - 0.1, rating_bins1 , 0.1, label='epoch 5')
+    plt.bar(X_axis + 0, rating_bins2 , 0.1, label='epoch 10')
+    plt.bar(X_axis + 0.1, rating_bins3 , 0.1, label='epoch 15')
+    plt.bar(X_axis + 0.2, rating_bins4 , 0.1, label='epoch 20')
+
+
+    plt.xticks(X_axis, X)
+    plt.xlabel("rating ")
+    plt.ylabel("frequency")
+    plt.title("ratings compared at each 5 iteration")
+    plt.legend()
+    plt.savefig('ratings.png')
+
 
 
 
